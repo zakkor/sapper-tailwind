b2396f15aaa127ef3781dddcf8fea003ebe8f379
Zakkor <edward.partenie@gmail.com>
2020-06-01 21:10:42 +0300

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>Sapper Tailwind project template</title>
+</svelte:head>
+
+<div class="bg-gray-400 p-4">
+	<h1 class="mb-4 text-2xl font-bold">Great success!</h1>
+	<p>Try editing this file (src/routes/index.svelte) to test live reloading.</p>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	// We are purging unused styles outside of tailwind, set to false to silence warning
+	purge: false,
+  theme: {
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+}
