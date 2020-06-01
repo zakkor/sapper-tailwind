7a80d81e0188a632ad2bd4e57f8e33410eae85c0
Zakkor <edward.partenie@gmail.com>
2020-06-01 21:45:51 +0300

    format config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
 	// We are purging unused styles outside of tailwind, set to false to silence warning
 	purge: false,
-  theme: {
-    extend: {}
-  },
-  variants: {},
-  plugins: []
+	theme: {
+		extend: {},
+	},
+	variants: {},
+	plugins: [],
 }
